2e94da8cb2152c496652b6a08c5e6267e27b6074
Tulio Ferreira <tuliof210@gmail.com>
2026-08-12 17:41:23 -0300

    chore(test): exigir 90% de cobertura e liberar noMagicNumbers nos testes

--- jest.config.ts
+++ jest.config.ts
@@ -14,6 +14,9 @@ const config = {
     "!src/generated/**",
     "!src/**/tests/**",
   ],
+  coverageThreshold: {
+    global: { statements: 90, branches: 90, functions: 90, lines: 90 },
+  },
 };
 
 export default createJestConfig(config);
